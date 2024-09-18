b19cdf6ce03dd903fb8a931133c00138a440bd09
photoplasm <fifamunk2004@gmail.com>
2024-09-18 14:54:08 +0700

    project form and js

---
+++ web-main/myapp/static/js/scripts.js
@@ -0,0 +1,33 @@
+function validateForm() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const remember = document.getElementById("remember").checked;
+    
+    const emailError = document.getElementById("email-error");
+    const passwordError = document.getElementById("password-error");
+    const checkedStatus = document.getElementById("checked-status");
+
+    emailError.textContent = "";
+    passwordError.textContent = "";
+    checkedStatus.textContent = "";
+
+    let isValid = true;
+
+    if (email == "" || !email.includes("@")) {
+        emailError.textContent = "Please enter a valid Email";
+        isValid = false;
+    }
+
+    if (password == "" || password.length < 8) {
+        passwordError.textContent = "Please enter password with at least 8 characters";
+        isValid = false;
+    }
+
+    if (remember) {
+        checkedStatus.textContent = "Checked";
+    } else {
+        checkedStatus.textContent = "Unchecked";
+    }
+
+    return isValid;
+}
